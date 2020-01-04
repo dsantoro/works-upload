f23d17434160978138c5259845e9f2cd0290a4a1
Danylo Santoro <danylo.santoro@gmail.com>
2020-01-04 23:09:27 +0000

    add dinamic port

--- src/index.js
+++ src/index.js
@@ -18,4 +18,4 @@ app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads', 'resiz
 
 app.use(require('./routes'));
 
-app.listen(3333, () => console.log("Server running..."));
+app.listen(process.env.PORT || 3333, () => console.log("Server running..."));
